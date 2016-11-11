8654c5d80b6d2bdf963e9d76cf91d48529269e2e
bryantee <bryantee@gmail.com>
2016-11-11 13:55:00 -0700

    Callback pattern demo, pre-promises

---
+++ promises/basic-callbacks.js
@@ -0,0 +1,20 @@
+function wait(seconds, callback) {
+  setTimeout(() => {
+    callback(new Date()); // <-- this is actually the function call
+  }, seconds * 1000);     // <-- second parameter of setTimeout function, seconds arg * 1000 ms
+}
+
+console.log('console log BEFORE calling wait:', new Date());
+
+wait(3, date => {   // <-- callback function declaration with 'date' as parameter
+  console.log('Console log inside 1st callback:', date);
+  wait(2, date => {
+    console.log('Console log inside 2nd callback:', date);
+    wait(5, date => {
+      console.log('console log inside 3rd callback:', date);
+    });
+  });
+});
+
+// This console log will be called before the calls inside the wait functions
+console.log('Console log AFTER calling wait:', new Date());
