af8044c954a32f833bdb81c2fe8c0aeff3360d9d
bryantee <bryantee@gmail.com>
2016-11-14 17:53:50 -0700

    Add functionality to query and return related artists from search
    Doesn't gracefully handle errors. Need to solve.

--- spotify-recommend/server.js
+++ spotify-recommend/server.js
@@ -31,7 +31,27 @@ app.get('/search/:name', (req, res) => {
 
   searchReq.on('end', item => {
     let artist = item.artists.items[0];
-    res.json(artist);
+    console.log(`Artist: ${artist.name}`);
+    let endpoint = 'artists/' + artist.id + '/related-artists';
+    console.log(`Endpoint: ${endpoint}`);
+    // setup new emitter object
+    let searchRelated = getFromApi(endpoint);
+    searchRelated.on('end', item => {
+      if (artist.name) {
+        console.log(`searchRelated called with: ${artist.name}`)
+      } else {
+        console.log('No response for artist.');
+      }
+      // add new property for 'related' which will be an array
+
+      artist.related = item.artists;
+      res.json(artist);
+    })
+    // NOTE: response.code passed as argument to 'error' event if response.ok != true
+    searchRelated.on('error', code => {
+      console.log(`Error, status code: ${code}`);
+      res.sendStatus(code);
+    });
   });
 
   searchReq.on('error', code => {
